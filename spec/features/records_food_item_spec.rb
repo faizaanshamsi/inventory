5c5ac3169c63b45f958f7ac4419c5803b0c8ec02
faizaanshamsi <faizaanshamsi@gmail.com>
2014-01-03 16:02:05 -0500

    write feature specs

---
+++ spec/features/records_food_item_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+feature "Records a food item", %q{
+  As a food service employee
+  I want to receive inventory
+  So that it can be recorded that we have food items
+  } do
+
+  # Acceptance Criteria:
+  #
+  # I must specify a title, description, and quantity of the food item
+  # If I do not specify the required information, I am prompted to fix errors and to resubmit
+  # If I specify the required information, my inventory entry is recorded.
+
+  context 'with correct information' do
+    it 'creates an entry' do
+      visit new_entry_path
+
+      fill_in "Title", with: "Sushi"
+      fill_in "Description", with: "Lots and lots of barbecue eel"
+      fill_in "Quantity", with: "1 barrel"
+      click_on "Create Entry"
+
+      expect(page).to have_content "Entry was successfully created"
+    end
+  end
+
+  context 'with incomplete information' do
+    it 'sends error and redirects to form' do
+      visit new_entry_path
+
+      click_on "Create Entry"
+
+      expect(page).to have_content "Title can't be blank"
+      expect(page).to have_content "Description can't be blank"
+      expect(page).to have_content "Quantity can't be blank"
+    end
+  end
+
+end
